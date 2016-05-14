e29066a9155ea72d1347d4ba96c93c45413454a2
bflemi3 <55v7wq5z>
2016-05-14 17:28:19 -0400

    moved everything into knife for now

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
 require.config({
-    baseUrl: 'lib'
+    baseUrl: 'node_modules'
 });
 
-require(["knife", "utils"], function(knife, _) {
+require(['meld/meld'], function(meld) {
 
-    var Person = knife('Person', function(name) {
+    // var Person = knife('Person', function(name) {
+    //     this.name = name;
+    //     this.walk = function(speed) {
+    //         var newSpeed = speed + 1;
+    //         _.log('new speed = {0}', newSpeed);
+    //         return newSpeed;
+    //     }
+    // }).before('walk', function(subject) {
+    //     // log arguments passed to walk invocation
+    //     _.log(subject.args);
+    //
+    //     // if the first argument is one, let's proceeed with original invocation but change the arguments
+    //     if(args[0] === 1)
+    //         subject.proceed(10);
+    //
+    //     // returning true will proceed the invocation with the original arguments
+    //     if(args[0] === 2)
+    //         return true;
+    //
+    //     // while returning false will halt execution of the method invocation
+    //     return false;
+    // });
+
+    function Person(name) {
         this.name = name;
         this.walk = function(speed) {
             var newSpeed = speed + 1;
-            _.log("new speed = {0}", newSpeed);
+            console.log('new speed = {0}', newSpeed);
             return newSpeed;
         }
-    }).before('walk', function(subject) {
-        // log arguments passed to walk invocation
-        _.log(subject.args);
+    }
 
-        // if the first argument is one, let's proceeed with original invocation but change the arguments
-        if(args[0] === 1)
-            subject.proceed(10);
+    var brandon = new Person('brandon');
 
-        // returning true will proceed the invocation with the original arguments
-        if(args[0] === 2)
-            return true;
-
-        // while returning false will halt execution of the method invocation
-        return false;
+    meld.before(brandon, 'walk', function(speed) {
+        console.log('here');
     });
 
+
     // let's test it out
-    var brandon = new Person("brandon");
+
     brandon.walk(5);
 });
